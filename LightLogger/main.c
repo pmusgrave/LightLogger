39e2a6f6f53d1278bb7a6dd46e2ce9afc49ee185
Paul Musgrave <ps.musgrave@gmail.com>
2017-08-19 17:55:42 -0400

    Initial commit

---
+++ LightLogger/main.c
@@ -0,0 +1,18 @@
+/*
+ * LightLogger.c
+ *
+ * Created: 8/19/2017 5:53:22 PM
+ * Author : paul
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
